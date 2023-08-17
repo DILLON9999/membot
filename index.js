bd0ee967922c0d2451d56fd0df5e8488518b6670
Dillon Timmer <dtimmer@sandiego.edu>
2023-08-17 09:45:24 -0700

    fix bot key

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ client.on('messageCreate', async message => {
     }
 });
 
-// // PROD BOT
-// client.login('MTEzOTY4MzMxNzMwNjg4ODE5Mw.Grljf1.J0LvZdZiEInMsbiS62QlxUKwH7_yHif6rwC2xs');
+// PROD BOT
+client.login('MTEzOTY4MzMxNzMwNjg4ODE5Mw.Grljf1.J0LvZdZiEInMsbiS62QlxUKwH7_yHif6rwC2xs');
 
-// TEST BOT
-client.login('MTEzOTI0NjY1ODA0ODIzMzYxNA.Gqo-JJ.chUss1NOEs5XoM_Pqchs9G2Km0O6rcw_TKqB0Q');
+// // TEST BOT
+// client.login('MTEzOTI0NjY1ODA0ODIzMzYxNA.Gqo-JJ.chUss1NOEs5XoM_Pqchs9G2Km0O6rcw_TKqB0Q');
