6eb46307efd7df8e0f5c977736d0fef0878bed84
Dillon Timmer <dtimmer@sandiego.edu>
2023-08-29 11:41:49 -0700

    implicit db name

--- database/database.js
+++ database/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectToDatabase = () => {
     // Connect to database ***UPDATE WITH MONGODB LOGIN***
-    mongoose.connect('mongodb+srv://membot:HO17rZFWx0XmFuHf@cluster0.txtdk0u.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect('mongodb+srv://membot:HO17rZFWx0XmFuHf@cluster0.txtdk0u.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => console.log('Connected to MongoDB'))
         .catch(err => console.error('Could not connect to MongoDB', err));
 }
