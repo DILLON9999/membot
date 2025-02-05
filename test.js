0b762732d60139a5acaa83a772816085004008d5
Dillon Timmer <dtimmer@sandiego.edu>
2025-02-05 00:10:46 -0800

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,1 @@
-const testObj = ['a']
 
-let index = 0
-testObj.splice(index, 1)
-
-if (testObj[index]) { console.log(testObj[index]) }
-else if (testObj[index-1]) { console.log(testObj[index-1])}
-else {console.log('nothing left in array')}
