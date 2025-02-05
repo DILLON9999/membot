c96d020d9b0ffc1dc1eace9f68277e58e9ae34d4
Dillon Timmer <dtimmer@sandiego.edu>
2025-02-05 00:12:10 -0800

    Update index.js

--- index.js
+++ index.js
@@ -13,12 +13,8 @@ client.on('messageCreate', async message => {
     if(message.content.startsWith('0x') && (message.content.split(' ')).length == 1 && !message.author.bot ){
         message["user"] = {"id": message.author.id}
         await buy(message, message.content)
-        // await purchase(message);
     }
 });
 
 // PROD BOT
-client.login('MTEzOTY4MzMxNzMwNjg4ODE5Mw.Grljf1.J0LvZdZiEInMsbiS62QlxUKwH7_yHif6rwC2xs');
-
-// // TEST BOT
-// client.login('MTEzOTI0NjY1ODA0ODIzMzYxNA.Gqo-JJ.chUss1NOEs5XoM_Pqchs9G2Km0O6rcw_TKqB0Q');
+client.login(process.env.DISCORD_BOT_TOKEN);
